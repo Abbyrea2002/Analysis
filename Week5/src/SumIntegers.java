767777fd44e6344f79b11442ad5e03979a7dab4f
Abbyrea2002 <abbyrea2002@gmail.com>
2024-10-22 13:36:34 +0100

    push

---
+++ Week5/src/SumIntegers.java
@@ -0,0 +1,51 @@
+/**
+ * Created by abbyr on 22/10/2024
+ * COMMENTS ABOUT PROGRAM HERE
+ */
+public class SumIntegers
+{
+   public static long sum_A(int n){
+      long sum = 0;
+      for(int i = 1; i<= n; i++){
+         sum = sum + i;
+      }
+       return sum;
+   }
+
+   public static long sum_B(int n){
+      long sum = 0;
+      for(int i = 1; i <= n; i++){
+         for(int j = 1; j <= i; j++){
+            sum = sum + 1;
+         }
+      }
+      return sum;
+   }
+
+   public static long sum_C(int n){
+      long sum = 0;
+      sum = n * (n + 1) / 2;
+      return sum;
+   }
+
+   public static void main(String[] args){
+      long startTime, endTime;
+
+      startTime = System.nanoTime();
+      System.out.println(sum_A(10000));
+      endTime = System.nanoTime();
+      System.out.println("Algorithm A: " +(endTime - startTime) + "ns\n");
+
+      startTime = System.nanoTime();
+      System.out.println(sum_B(10000));
+      endTime = System.nanoTime();
+      System.out.println("Algorithm B: " +(endTime - startTime) + "ns\n");
+
+      startTime = System.nanoTime();
+      System.out.println(sum_C(10000));
+      endTime = System.nanoTime();
+      System.out.println("Algorithm C: " +(endTime - startTime) + "ns\n");
+
+
+   }
+}//class
